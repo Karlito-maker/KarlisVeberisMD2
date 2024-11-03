1cc0a5bc8883a5be7bb07cc999f5426cd1ab168e
Karlito-maker <karliberito@gmail.com>
2024-11-03 23:58:52 +0200

    Update Assignment.cs

--- Assignment.cs
+++ Assignment.cs
@@ -1,22 +1,67 @@
-public class Assignment
+using System;
+using System.ComponentModel;
+
+public class Assignment : INotifyPropertyChanged
 {
-    // Uzdevuma termins
-    public DateTime Deadline { get; set; }
-    //Kurs, kura ir jaizpilda uzd
-    public Course Course { get; set; }
-    //uzdevuma apraksts
-    public string Description { get; set; }
-    //Konstruktors, kas uzstada ipasibas
+    private DateTime _deadline;
+    private Course _course;
+    private string _description;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public DateTime Deadline
+    {
+        get => _deadline;
+        set
+        {
+            if (_deadline != value)
+            {
+                _deadline = value;
+                OnPropertyChanged(nameof(Deadline));
+            }
+        }
+    }
+
+    public Course Course
+    {
+        get => _course;
+        set
+        {
+            if (_course != value)
+            {
+                _course = value;
+                OnPropertyChanged(nameof(Course));
+            }
+        }
+    }
+
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            if (_description != value)
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+    }
+
     public Assignment(DateTime deadline, Course course, string description)
     {
         Deadline = deadline;
         Course = course;
         Description = description;
     }
-    //Atgriez info ka tekstu
+
     public override string ToString()
     {
         return $"Assignment: {Description}, Deadline: {Deadline}, Course: {Course.Name}";
     }
-}
 
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
